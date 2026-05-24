714af36a6773903ff551971b0676e0946c811799
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-05-24 17:35:52 +0530

    feat: Add abstract methods to implement business logic

--- src/com/stchool/ecommerce/service/ProductService.java
+++ src/com/stchool/ecommerce/service/ProductService.java
@@ -4,10 +4,34 @@ import com.stchool.ecommerce.exception.ProductNotFoundException;
 import com.stchool.ecommerce.model.Product;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public interface ProductService {
     List<Product> getProductsByAvailability(boolean isAvailable);
     List<Product> getProductsByCategory(String category);
     Product getProductById(int id) throws ProductNotFoundException;
     List<String> getAllCategories();
+    List<Product> getProductsAbovePrice(double price);
+    List<String> getAllProductNames();
+    long getAvailableProductCount();
+    boolean existsByCompany(String company);
+    boolean areAllProductsAvailable();
+    Optional<Product> getFirstProduct();
+    List<Product> getTopNExpensiveProducts(int n);
+    List<Product> sortProductsByPriceAsc();
+    List<Product> sortProductsByPriceDesc();
+    double getTotalInventoryValue();
+    double getTotalPriceAfterDiscount();
+    List<Product> getProductsManufacturedAfter(int year);
+    List<Product> getAvailableProductsAbovePrice(double price);
+    Map<String, Long> getProductCountByCategory();
+    Map<String, List<Product>> groupProductsByCategory();
+    Map<String, List<Product>> groupProductsByCompany();
+    Map<Boolean, List<Product>> partitionProductsByAvailability();
+    Optional<Product> getMostExpensiveProduct();
+    Optional<Product> getCheapestProduct();
+    Map<Integer, Product> mapProductsById();
+    Map<String, Double> getAveragePriceByCategory();
+    Map<String, List<Product>> getTop3ExpensiveProductsByCategory();
 }
